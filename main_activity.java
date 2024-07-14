5238e898428b96770e8572d018c29a10eb028ba7
Tit_us <145954593+Titus1122@users.noreply.github.com>
2024-07-14 21:48:45 +0530

    Create main_activity.java

---
+++ main_activity.java
@@ -0,0 +1,83 @@
+package com.example.simplecalculator;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonAdd, buttonSub, buttonMul, buttonDiv;
+    EditText editTextN1, editTextN2;
+    TextView textView;
+    int num1, num2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+            buttonAdd = findViewById(R.id.btn_add);
+            buttonSub = findViewById(R.id.btn_sub);
+            buttonMul = findViewById(R.id.btn_mul);
+            buttonDiv = findViewById(R.id.btn_div);
+            editTextN1 = findViewById(R.id.number1);
+            editTextN2 = findViewById(R.id.number2);
+            textView = findViewById(R.id.Answer);
+
+
+            buttonAdd.setOnClickListener(this);
+            buttonSub.setOnClickListener(this);
+            buttonMul.setOnClickListener(this);
+            buttonDiv.setOnClickListener(this);
+
+
+
+    }
+
+        public int getIntFromEditText(EditText editText) {
+            if (editText.getText().toString().equals("")) {
+                Toast.makeText(this, "Enter Number", Toast.LENGTH_SHORT).show();
+                return 0;
+            } else {
+                return Integer.parseInt(editText.getText().toString());
+            }
+    }
+
+    @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
+    @Override
+    public void onClick(View view) {
+        num1 = getIntFromEditText(editTextN1);
+        num2 = getIntFromEditText(editTextN2);
+        if (view.getId() == R.id.btn_add) {
+            textView.setText("Answer = " + (num1 + num2));
+
+        }
+        if (view.getId() == R.id.btn_sub) {
+            textView.setText("Answer = " + (num1 - num2));
+
+        }
+        if (view.getId() == R.id.btn_mul) {
+            textView.setText("Answer = " + (num1 * num2));
+
+        }
+        if (view.getId() == R.id.btn_div) {
+            textView.setText("Answer = " + ((float)num1 / (float) num2));
+
+        }
+
+
+
+
+    }
+}
